62119bbd80c19c9ddf3f3021502596f5ec2625fd
STROAD <koseok0611@naver.com>
2022-04-16 09:49:03 +0900

    기본 화면 설정

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import pygame
 from pygame.locals import *
 import math
+
+
+pygame.init()
+pygame.display.set_caption("포물선 운동 시뮬레이션")
+
+# 스크린 사이즈
+screen_width = 1920
+screen_height = 1080
+
+# screen_width x screen_height 사이즈의 스크린 표면을 만듦
+screen = pygame.display.set_mode((screen_width, screen_height))
