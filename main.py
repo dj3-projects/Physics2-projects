0dbb023ef0093ee0de0e6ab04639fe0b0d3aeeff
STROAD <koseok0611@naver.com>
2022-05-05 19:49:08 +0900

    tkinter.ttk import 취소

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from tkinter import *
-from tkinter.ttk import *
-
 from matplotlib import *
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.backend_bases import key_press_handler
