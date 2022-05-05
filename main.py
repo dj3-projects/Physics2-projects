2cfbe2fdf08ed5951ba89d780785c22e401a0fa0
STROAD <koseok0611@naver.com>
2022-05-05 13:03:58 +0900

    기본 창 생성

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ from matplotlib import *
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
+
+
+root = Tk()
+root.title("포물선 운동 시뮬레이션")
+root.geometry("1080x720")
+
+
+root.mainloop()
