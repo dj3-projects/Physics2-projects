0dae569f8e4cdf73f8c9dfcd56084d4cde559212
STROAD <koseok0611@naver.com>
2022-05-07 17:19:42 +0900

    그래프가 나올 프레임 추가

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ from matplotlib.figure import Figure
 root = Tk()
 root.title("포물선 운동 시뮬레이션")
 root.geometry("1080x720")
-root.state("zoomed")
+
+
+graph_frame = Frame(root, relief="solid", bd=3, bg="white")
+graph_frame.grid(row=0, column=0, rowspan=2, columnspan=2, sticky="nsew")
 
 
 root.mainloop()
