c95ff8683a049457a4a7b693370bddb11f48c0d4
STROAD <koseok0611@naver.com>
2022-05-07 17:20:12 +0900

    정보가 출력될 프레임 추가

--- main.py
+++ main.py
@@ -13,5 +13,8 @@ root.geometry("1080x720")
 graph_frame = Frame(root, relief="solid", bd=3, bg="white")
 graph_frame.grid(row=0, column=0, rowspan=2, columnspan=2, sticky="nsew")
 
+info_frame = Frame(root, width=230, bg="white")
+info_frame.grid(row=0, column=2, columnspan=2, sticky="nsew")
+
 
 root.mainloop()
