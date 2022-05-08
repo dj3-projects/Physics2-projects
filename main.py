7671e6976e27c30e7159406c06b5bf08d7f8017c
STROAD <koseok0611@naver.com>
2022-05-08 12:04:06 +0900

    (공의) 속도 레이블 & 엔트리 추가

--- main.py
+++ main.py
@@ -31,6 +31,14 @@ lb_t.grid(row=0, column=0, pady=15)
 e_t.grid(row=0, column=1, pady=15)
 lb_tu.grid(row=0, column=2, pady=15)
 
+# (공의) 속도 레이블 & 엔트리
+lb_v = Label(info_frame, text="      속도 :", bg="white", font=(None, 15))
+e_v = Entry(info_frame, width=6, bg="white", bd=0, justify="right", font=(None, 15))
+lb_vu = Label(info_frame, text="m/s", bg="white", font=(None, 15))
+lb_v.grid(row=1, column=0, pady=15)
+e_v.grid(row=1, column=1, pady=15)
+lb_vu.grid(row=1, column=2, pady=15)
+
 
 # 각 프레임의 크기를 창 크기가 변할때 같이 일정하게 변하도록함
 root.grid_rowconfigure(0, weight=1)
