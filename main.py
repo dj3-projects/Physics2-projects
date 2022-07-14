c517bebe678d12f410e85b3a6ce40fddc9b00200
STROAD <koseok0611@naver.com>
2022-07-14 20:48:41 +0900

    .

--- main.py
+++ main.py
@@ -2,21 +2,18 @@ from sys import setrecursionlimit
 from tkinter import *
 import time
 from ctypes import windll
-from matplotlib.pyplot import clf, ion, subplots, xlim, ylim, isinteractive
+from matplotlib.pyplot import cla, plot, scatter, subplots, isinteractive
 import numpy as np
 from matplotlib import *
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-from matplotlib import font_manager, rc
 import matplotlib.font_manager as fm
-from matplotlib.animation import FuncAnimation
-from matplotlib.figure import Figure
 
 
 setrecursionlimit(10**6)
 
 
 # matplotlib font 설정
-path = "Pretendard-Medium.ttf"
+path = "C:\Workspace\parabolic-motion-simulation\Pretendard-Medium.ttf"
 fontprop = fm.FontProperties(fname=path)
 
 params = {"xtick.labelsize": 20, "ytick.labelsize": 20}
@@ -24,13 +21,13 @@ rcParams.update(params)
 
 
 root = Tk()
-root.title("포물선 운동 시뮬레이션")
+root.wm_title("포물선 운동 시뮬레이션")
 
 # 해상도 구하기
 width_px = root.winfo_screenwidth()
 user32 = windll.user32
 user32.SetProcessDPIAware()
-[w, h] = [user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)]
+w = user32.GetSystemMetrics(0)
 
 # 해상도에 따라 창크기 맞춤
 if w == 3840:
@@ -44,57 +41,65 @@ else:
 
 
 def para_clac():
-    global v0, ang, mg, r, h, t
-
     v0 = float(e_v0.get())
     ang = np.deg2rad(int(e_ang.get()))
     mg = float(e_mg.get())
 
     r = int(v0**2 * np.sin(2 * ang) / mg)
-    h = int(v0**2 * (np.sin(ang)) ** 2 / 2 * mg)
+    h = int(v0**2 * ((np.sin(ang)) ** 2) / 2 * mg)
 
     t_flight = 2 * v0 * np.sin(ang) / mg
     t = np.linspace(0, t_flight, 100, dtype=float)
 
+    return v0, ang, mg, r, h, t_flight, t
+
 
 # 실시간으로 포물선을 그리는 함수
-def para_plot(n):
+def para_plot():
     global line, canvas
 
-    dt = 0
+    # dt = 0
+    e_bh.delete(0, "end")
+    e_br.delete(0, "end")
 
-    para_clac()
+    v0, ang, mg, r, h, t_flight, t = para_clac()
 
-    xlim(0, r + 2)
-    ylim(0, h + 2)
+    e_bh.insert(0, h)
+    e_br.insert(0, r)
+
+    def x(tt):
+        return v0 * np.cos(ang) * tt
+
+    def y(tt):
+        return v0 * np.sin(ang) * t - 0.5 * mg * tt**2
+
+    t = np.linspace(0, t_flight, 100000, dtype=float)
+
+    plot(x(t), y(t))
 
     figure.canvas.draw()
     figure.canvas.flush_events()
 
-    for n in range(100):
-        e_t.delete(0, "end")
-        dt = t[n]
-        e_t.insert(0, dt)
+    # for n in range(100):
+    #     e_t.delete(0, "end")
+    #     dt = t[n]
+    #     e_t.insert(0, dt)
 
-        ux = float(v0 * np.cos(ang))
-        uy = v0 * np.sin(ang) - mg * dt
+    #     ux = float(v0 * np.cos(ang))
+    #     uy = v0 * np.sin(ang) - mg * dt
 
-        e_v.delete(0, "end")
-        v = np.round(((ux**2 + uy**2) ** 0.5), 3)
-        e_v.insert(0, v)
+    #     e_v.delete(0, "end")
+    #     v = np.round(((ux**2 + uy**2) ** 0.5), 3)
+    #     e_v.insert(0, v)
 
-        parax = np.round((v0 * np.cos(ang) * dt), 3)
-        paray = np.round((v0 * np.sin(ang) * dt - 0.5 * mg * dt**2), 3)
+    #     parax = np.round((v0 * np.cos(ang) * dt), 3)
+    #     paray = np.round((v0 * np.sin(ang) * dt - 0.5 * mg * dt**2), 3)
 
-        line.set_data(parax, paray)
-
-        figure.canvas.draw()
-        figure.canvas.flush_events()
-        time.sleep(0.1)
+    #     time.sleep(0.1)
 
 
-def plot():
-    para_plot(0)
+def run():
+    para_plot()
 
 
 # 그래프가 출력될 프레임
@@ -113,12 +118,13 @@ input_frame.grid(row=1, column=2, columnspan=2, sticky="nsew")
 # 그래프
 # fig = Figure()
 
-isinteractive()
+# isinteractive()
 
 figure, ax = subplots()
 ax.set_title("포물선 운동 시물레이션", fontproperties=fontprop, fontsize=30)
 ax.set_xlabel("x", fontsize=20)
 ax.set_ylabel("y", fontsize=20)
+ax.grid(True)
 
 (line,) = ax.plot([], [])
 
@@ -136,37 +142,37 @@ figure.canvas.get_tk_widget().grid(row=0, column=0, sticky="nsew")
 # canvas.draw()
 
 
-# (발사 후 흐른) 시간 레이블 & 엔트리
-lb_t = Label(info_frame, text="      시간 :", bg="white", font=(None, 15))
-e_t = Entry(info_frame, width=6, bg="white", bd=0, justify="right", font=(None, 15))
-lb_tu = Label(info_frame, text=" s", bg="white", font=(None, 15))
-lb_t.grid(row=0, column=0, pady=20)
-e_t.grid(row=0, column=1, pady=20)
-lb_tu.grid(row=0, column=2, pady=20)
+# # (발사 후 흐른) 시간 레이블 & 엔트리
+# lb_t = Label(info_frame, text="      시간 :", bg="white", font=(None, 15))
+# e_t = Entry(info_frame, width=6, bg="white", bd=0, justify="right", font=(None, 15))
+# lb_tu = Label(info_frame, text=" s", bg="white", font=(None, 15))
+# lb_t.grid(row=0, column=0, pady=20)
+# e_t.grid(row=0, column=1, pady=20)
+# lb_tu.grid(row=0, column=2, pady=20)
 
-# (공의) 속도 레이블 & 엔트리
-lb_v = Label(info_frame, text="      속도 :", bg="white", font=(None, 15))
-e_v = Entry(info_frame, width=6, bg="white", bd=0, justify="right", font=(None, 15))
-lb_vu = Label(info_frame, text="m/s", bg="white", font=(None, 15))
-lb_v.grid(row=1, column=0, pady=20)
-e_v.grid(row=1, column=1, pady=20)
-lb_vu.grid(row=1, column=2, pady=20)
+# # (공의) 속도 레이블 & 엔트리
+# lb_v = Label(info_frame, text="      속도 :", bg="white", font=(None, 15))
+# e_v = Entry(info_frame, width=6, bg="white", bd=0, justify="right", font=(None, 15))
+# lb_vu = Label(info_frame, text="m/s", bg="white", font=(None, 15))
+# lb_v.grid(row=1, column=0, pady=20)
+# e_v.grid(row=1, column=1, pady=20)
+# lb_vu.grid(row=1, column=2, pady=20)
 
 # 공의 높이 레이블 & 엔트리
 lb_bh = Label(info_frame, text="      높이 :", bg="white", font=(None, 15))
 e_bh = Entry(info_frame, width=6, bg="white", bd=0, justify="right", font=(None, 15))
 lb_bhu = Label(info_frame, text="m", bg="white", font=(None, 15))
-lb_bh.grid(row=2, column=0, pady=20)
-e_bh.grid(row=2, column=1, pady=20)
-lb_bhu.grid(row=2, column=2, pady=20)
+lb_bh.grid(row=0, column=0, pady=20)
+e_bh.grid(row=0, column=1, pady=20)
+lb_bhu.grid(row=0, column=2, pady=20)
 
 # 공이 날라간 거리 레이블 & 엔트리
 lb_br = Label(info_frame, text="      거리 :", bg="white", font=(None, 15))
 e_br = Entry(info_frame, width=6, bg="white", bd=0, justify="right", font=(None, 15))
 lb_bru = Label(info_frame, text="m", bg="white", font=(None, 15))
-lb_br.grid(row=3, column=0, pady=20)
-e_br.grid(row=3, column=1, pady=20)
-lb_bru.grid(row=3, column=2, pady=20)
+lb_br.grid(row=1, column=0, pady=20)
+e_br.grid(row=1, column=1, pady=20)
+lb_bru.grid(row=1, column=2, pady=20)
 
 # (공의) 발사(초기)속도 레이블 & 엔트리
 lb_v0 = Label(input_frame, text="발사속도 :", bg="white", font=(None, 15))
@@ -205,7 +211,7 @@ button = Button(
     bg="white",
     activebackground="white",
     font=(None, 15),
-    command=plot,
+    command=run,
 )
 button.grid(row=3, column=0, columnspan=3, pady=20)
 
